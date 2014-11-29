e721009184a272de2c8060e8d69e8634519024d2
Nicolás Parada <shinobi-san@outlook.com>
2014-11-29 09:07:54 -0300

    Added some compilation options

--- index.js
+++ index.js
@@ -7,13 +7,19 @@ var elixir       = require('laravel-elixir'),
 	rename       = require('gulp-rename'),
 	notify       = require('gulp-notify'),
 	utilities    = require('laravel-elixir/ingredients/helpers/utilities'),
-	config       = require('laravel-elixir').config;
+	config       = require('laravel-elixir').config,
+	_            = require('underscore');;
 
-elixir.extend('rubySass', function(src, output) {
+elixir.extend('rubySass', function(src, output, options) {
 
 	var config = this;
 	var baseDir = config.assetsDir + 'sass';
 	src = utilities.buildGulpSrc(src, baseDir, '**/*.+(sass|scss)');
+
+	options = _.extend({
+		"sourcemap=none": true,
+		includePaths: [config.bowerDir + "/bootstrap-sass-official/assets/stylesheets"]
+	}, options);
 
 	gulp.task('rubySass', function() {
 		var onError = function(err) {
@@ -28,19 +34,11 @@ elixir.extend('rubySass', function(src, output) {
 		};
 
 		return gulp.src(src)
-			.pipe(rubySass({
-				"sourcemap=none": true
-			})).on('error', onError)
+			.pipe(rubySass(options)).on('error', onError)
 			.pipe(autoprefixer())
 			.pipe(gulpif(config.production, minify()))
 			.pipe(gulpif(config.production, rename({suffix: '.min'})))
-			.pipe(gulp.dest(output || config.cssOutput))
-			/*.pipe(notify({
-				title: 'Laravel Elixir',
-				subtitle: 'Stylus Compiled!',
-				icon: __dirname + '/../laravel-elixir/icons/laravel.png',
-				message: ' '
-			}))*/;
+			.pipe(gulp.dest(output || config.cssOutput));
 	});
 
 	this.registerWatcher('rubySass', baseDir + '/**/*.+(sass|scss)');
