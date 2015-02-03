1f0d915d6c28efa40b0bfc401c6afbb8f9649002
Thijs Scheepers <tscheepers@users.noreply.github.com>
2015-02-03 11:24:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,21 @@
 var elixir       = require('laravel-elixir'),
 	gulp         = require('gulp'),
 	rubySass     = require('gulp-ruby-sass'),
-	gulpif       = require('gulp-if'),
-	autoprefixer = require('gulp-autoprefixer'),
-	minify       = require('gulp-minify-css'),
-	Notification = require('laravel-elixir/ingredients/commands/Notification'),
-	utilities    = require('laravel-elixir/ingredients/commands/Utilities'),
-	config       = require('laravel-elixir').config,
-	_            = require('underscore');;
 
 elixir.extend('rubySass', function(src, output, options) {
 
-	var config = this;
-	var baseDir = config.assetsDir + 'sass';
-	src = utilities.buildGulpSrc(src, baseDir, '**/*.+(sass|scss)');
+    options = _.extend({
+        outputStyle: inProduction ? 'compressed' : 'nested',
+        includePaths: [elixir.config.bowerDir + "/bootstrap-sass-official/assets/stylesheets"]
+    }, options);
 
-	options = _.extend({
-		"sourcemap=none": true,
-		loadPath: [config.bowerDir, config.bowerDir + "/bootstrap-sass-official/assets/stylesheets"]
-	}, options);
-
-	gulp.task('rubySass', function() {
-		var onError = function(err) {
-			new Notification().error(err, 'Sass Compilation Failed!');
-			this.emit('end');
-		};
-
-		return gulp.src(src)
-			.pipe(rubySass(options)).on('error', onError)
-			.pipe(autoprefixer())
-			.pipe(gulpif(config.production, minify()))
-			.pipe(gulp.dest(output || config.cssOutput));
-	});
-
-	this.registerWatcher('rubySass', baseDir + '/**/*.+(sass|scss)');
-	return this.queueTask('rubySass');
+    return compile({
+        compiler: 'RubySass',
+        pluginName: 'ruby-sass',
+        pluginOptions: options,
+        src: src,
+        output: output,
+        search: '**/*.+(sass|scss)'
+    });
 
 });
