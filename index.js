0ebd4fd21670135b4a1cb729a3aebfb9024200d1
Thijs Scheepers <tscheepers@users.noreply.github.com>
2015-02-03 12:05:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
 var elixir = require('laravel-elixir');
-var compile = require('laravel-elixir/ingredients/commands/CompileCSS');
+var plugins = require('gulp-load-plugins')();
+var utilities = require('laravel-elixir/ingredients/commands/Utilities');
+var Notification = require('laravel-elixir/ingredients/commands/Notification');
 var _ = require('underscore');
 
-var inProduction = elixir.config.production
+var inProduction = elixir.config.production;
 
 elixir.extend('rubySass', function(src, output, options) {
 
@@ -11,6 +14,36 @@ elixir.extend('rubySass', function(src, output, options) {
         outputStyle: inProduction ? 'compressed' : 'nested',
         includePaths: [elixir.config.bowerDir + "/bootstrap-sass-official/assets/stylesheets"]
     }, options);
+
+    function compile(options) {
+        var src = utilities.buildGulpSrc(
+            options.src, config.assetsDir + options.pluginName, options.search
+        );
+
+        var onError = function(e) {
+            new Notification().error(e, options.compiler + ' Compilation Failed!');
+
+            this.emit('end');
+        };
+
+        process.stdout.write(Object.getOwnPropertyNames(plugins) + '\n\n');
+
+        gulp.task(options.pluginName, function() {
+            return gulp.src(src)
+                .pipe(plugins[options.pluginName](options.pluginOptions)).on('error', onError)
+                .pipe(plugins.autoprefixer())
+                .pipe(plugins.if(config.production, plugins.minifyCss()))
+                .pipe(gulp.dest(options.output || config.cssOutput))
+                .pipe(new Notification().message(options.compiler + ' Compiled!'));
+        });
+
+        config.registerWatcher(
+            options.pluginName,
+            config.assetsDir + options.pluginName + '/' + options.search
+        );
+
+        return config.queueTask(options.pluginName);
+    }
 
     return compile({
         compiler: 'RubySass',
